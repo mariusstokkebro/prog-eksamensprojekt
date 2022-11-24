b83761a6ae3af607a9c09dccdac58301491aa856
H4xx0r9 <orfeltmads@gmail.com>
2022-11-24 14:10:49 +0100

    rykket main ind i view

--- Controller/Film.java
+++ Controller/Film.java
@@ -1,5 +1,33 @@
 package Controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Film {
+    private String name;
+    private int year;
+    private String genre;
+    private double rating;
+    private List<Film> films = new ArrayList<>();
+    public Film(String name, int year,String genre, double rating){
+        this.name = name;
+        this.year = year;
+        this.genre = genre;
+        this.rating = rating;
+    }
+    public String getName(){
+        return name;
+    }
+    public int getYear(){
+        return year;
+    }
+    public String getGenre(){
+        return genre;
+    }
+    public double getRating(){
+        return rating;
+    }
+
+
 
 }
