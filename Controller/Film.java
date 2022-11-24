111d1b799b6513418bb45c9e0a71328556383104
Slasix <Casper.Pilgaard@hotmail.com>
2022-11-24 14:18:11 +0100

    ændret genre til array

--- Controller/Film.java
+++ Controller/Film.java
@@ -6,13 +6,13 @@ import java.util.List;
 public class Film {
     private String name;
     private int year;
-    private String genre;
+    private String[] genre;
     private double rating;
     private List<Film> films = new ArrayList<>();
     public Film(String name, int year,String genre, double rating){
         this.name = name;
         this.year = year;
-        this.genre = genre;
+        this.genre = genre.trim().split(",");
         this.rating = rating;
     }
     public String getName(){
@@ -21,7 +21,7 @@ public class Film {
     public int getYear(){
         return year;
     }
-    public String getGenre(){
+    public String[] getGenre(){
         return genre;
     }
     public double getRating(){
