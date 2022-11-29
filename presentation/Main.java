49a72766b4a983de5bda100afddb4ed582b45856
Slasix <Casper.Pilgaard@hotmail.com>
2022-11-29 13:02:22 +0100

    rettet i noget data

--- presentation/Main.java
+++ presentation/Main.java
@@ -6,6 +6,8 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
 
+        src.Media media = new src.Media();
+
 
     }
     //kode her og her
--- src/Media.java
+++ src/Media.java
@@ -6,31 +6,54 @@ import java.util.List;
 
 public class Media {
 
-    public void medie() {
+    List<Film> films;
+    List<Serie> series;
+    List<String> media;
+    public Media() {
 
-    Data.DataAccess films = new Data.DataAccess("/Users/casperpilgaard/Downloads/Data/film.txt");
-    Data.DataAccess series = new Data.DataAccess("/Users/casperpilgaard/Downloads/Data/serier.txt");
+        List<Film> films = new ArrayList<>();
+        List<Serie> serie = new ArrayList<>();
+
+    Data.DataAccess filmsData = new Data.DataAccess("/Users/casperpilgaard/Downloads/Data/film.txt");
+    Data.DataAccess seriesData = new Data.DataAccess("/Users/casperpilgaard/Downloads/Data/serier.txt");
 
     List<String> media = new ArrayList<String>();
 
-    List<String> filmData = films.load();
+    List<String> filmData = filmsData.load();
         for (String element : filmData) {
         String[] line = element.split(";");
         String movieName = line[0];
         int year = Integer.parseInt(line[1].trim());
         String genre = line[2];
         double rating = Double.parseDouble(line[3].trim().replace(",", "."));
+        Film film1 = new Film(movieName, year, genre, rating);
 
-        System.out.println(movieName + " " + year + " " + genre + " " + rating);
-        media.add(element);
-    }
-
-    List<String> seriesData = series.load();
-
-
-
-        Collections.sort(media);
+        films.add(film1);
 
     }
+
+    List<String> serieData = seriesData.load();
+        for (String element : serieData) {
+            String[] line = element.split(";");
+            String seriesName = line[0];
+            int year = Integer.parseInt(line[1].trim());
+            String genre = line[2];
+            double rating = Double.parseDouble(line[3].trim().replace(",", "."));
+            String episodes = line[4];
+            Serie serie1 = new Serie(seriesName, year, genre, rating, episodes);
+
+            serie.add(serie1);
+
+
+        }
+
+        for (Film film : films) {
+            System.out.println(film.getName());
+        }
+
+
+    }
+
+
 
 }
