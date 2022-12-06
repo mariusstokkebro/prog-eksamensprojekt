89d9ea57bfe9a5b8716a7b038e8d83b3b5eac79a
H4xx0r9 <orfeltmads@gmail.com>
2022-12-06 15:44:24 +0100

    rykket main ind i view

--- Source/presentation/Main.java
+++ Source/presentation/Main.java
@@ -1,13 +1,14 @@
 package presentation;
 
+import Data.Image;
 import src.Media;
 
 public class Main {
     public static void main(String[] args) {
 
-        Display display = new Display();
+        //Display display = new Display();
         Media media = new Media();
-
+        //Image i = new Image();
     }
     //kode her og her
 }
