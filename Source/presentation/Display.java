4a46a70fb17dfd12f0c1feb093bc3799819d2e25
Slasix <Casper.Pilgaard@hotmail.com>
2022-12-08 16:58:23 +0100

    ...

--- Source/presentation/Display.java
+++ Source/presentation/Display.java
@@ -14,6 +14,7 @@ import java.nio.Buffer;
 import java.util.ArrayList;
 import javax.imageio.ImageIO;
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.util.List;
 
 public class Display{
@@ -30,76 +31,103 @@ List<Medier> seriesList;
 
     }
 
-    JButton makeButton(String text, int posx, int posy, int height, int width, int fontSize, Color color) {
+    JButton makeButton(String text, int height, int width, int fontSize, Color color) {
         JButton button = new JButton(text);
         button.setForeground(color);
-        button.setBounds(posx, posy, height, width);
         button.setBorder(null);
+        button.setSize(height, width);
         button.setContentAreaFilled(false);
         button.setFont(button.getFont().deriveFont(0, fontSize));
         return button;
 
     }
 
-    void makeAllPosters(List<Medier> list, JPanel panel) {
+    void makeAllPosters(List<Medier> list, JPanel panel, GridBagConstraints cons) {
 
-
-        int posx = 80;
-        int posy = 0;
+        int posx = 0;
+        int posy = 2;
 
         for (int i = 0; i < list.size(); i++) {
             System.out.println(list.get(i).getName());
-            if (i % 7 == 0) {
-                posx = 80;
-                posy += 200;
+            if ((i % 7 == 0) && i!=0) {
+                posx = 0;
+                posy++;
+
             }
             ImageIcon img = new ImageIcon(getClass().getResource("/" + list.get(i).getName()+".jpg"));
             JButton poster = new JButton(img);
             poster.setBorder(null);
             poster.setContentAreaFilled(false);
-            poster.setBounds(posx, posy, 100, 150);
-            panel.add(poster);
-            posx += 200;
+            cons.gridx = posx;
+            cons.gridy = posy;
+            panel.add(poster, cons);
+            posx++;
+
         }
     }
 
     void homeScreen() {
         JFrame frame = new JFrame("Popkorn tid");
-        JPanel jp1 = new JPanel();
-
-
+        JPanel mainPanel = new JPanel(new GridBagLayout());
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        jp1.setBackground(Color.black);
-
-        jp1.setLayout(null);
 
 
-        frame.setSize(1980, 1080);
 
-        frame.add(jp1);
+        frame.setBackground(Color.black);
+
+
+        GridBagConstraints constraints = new GridBagConstraints();
+        constraints.anchor = GridBagConstraints.NORTHWEST;
+        constraints.fill = GridBagConstraints.HORIZONTAL;
+        constraints.weighty = 0.1;
+        constraints.weightx = 1;
+        constraints.insets = new Insets(5, 5, 5, 5);
+
+        mainPanel.setBackground(Color.black);
+
+        GridBagConstraints cons = new GridBagConstraints();
+        cons.anchor = GridBagConstraints.NORTH;
+        cons.fill = GridBagConstraints.HORIZONTAL;
+        cons.insets = new Insets(5, 5, 5, 5);
+
+        frame.setSize(800, 800);
+
+
+
+        frame.add(mainPanel);
+
 
 
 
         //Medier knap
-        JButton but1 = makeButton("Medier", 50, 50, 100, 50, 20, Color.red);
-        jp1.add(but1);
-        //Medier knap
+        JButton but1 = makeButton("Medier", 50, 25, 25, Color.red);
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+        constraints.anchor = GridBagConstraints.FIRST_LINE_START;
+
+        mainPanel.add(but1, constraints);
 
         //Film knap
-        JButton but2 = makeButton("Film", 200, 50, 100, 50, 20, Color.red);
-        jp1.add(but2);
+        JButton but2 = makeButton("Film", 50, 25, 25, Color.red);
+        constraints.gridx = 1;
+        constraints.gridy = 0;
+        mainPanel.add(but2, constraints);
 
         //Serier knap
-        JButton but3 = makeButton("Serier", 350, 50, 100, 50, 20, Color.red);
-        jp1.add(but3);
+        JButton but3 = makeButton("Serier", 50, 25, 25, Color.red);
+        constraints.gridx = 2;
+        constraints.gridy = 0;
+        mainPanel.add(but3, constraints);
 
 
         //Titel med 'Popkorn tid'
         JLabel title = new JLabel("Popkorn Tid");
         title.setForeground(Color.gray);
-        title.setBounds(630, 30, 300, 50);
-        title.setFont(title.getFont().deriveFont(0, 30));
-        jp1.add(title);
+        title.setFont(title.getFont().deriveFont(0, 25));
+        constraints.gridx = 3;
+        constraints.gridy = 0;
+
+        mainPanel.add(title, constraints);
 
         //Titel billede
         BufferedImage popcornimg = null;
@@ -109,35 +137,33 @@ List<Medier> seriesList;
             e.printStackTrace();
         }
 
+        //Søgefelt
+
+
+
         Image popcornimg2 = popcornimg.getScaledInstance(96, 96, 4);
 
         ImageIcon popcornicon = new ImageIcon(popcornimg2);
         JLabel popcorn = new JLabel(popcornicon);
-        popcorn.setBounds(810, 20, 96, 96);
-        jp1.add(popcorn);
+        constraints.gridx = 4;
+        constraints.gridy = 0;
+        mainPanel.add(popcorn, cons);
 
 
 
         //Filmplakater
-        makeAllPosters(filmList, jp1);
+        makeAllPosters(mediaList, mainPanel, constraints);
+
+
 
         //Scrollbar
-        /*JScrollPane scrollbar = new JScrollPane(jp1);
-        scrollbar.setVerticalScrollBarPolicy(scrollbar.VERTICAL_SCROLLBAR_ALWAYS);
-        jp1.add(scrollbar, BorderLayout.EAST);*/
+        JScrollPane sp = new JScrollPane(mainPanel);
+        sp.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        sp.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        frame.add(sp);
+
         frame.setVisible(true);
-
-
-
-
-
-
-
     }
-
-
-
-
 
     void titleScreen() {
         JFrame openScreen = new JFrame("Popkorn Tid");
@@ -145,26 +171,20 @@ List<Medier> seriesList;
 
         openScreen.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
-
-
         JPanel panel = new JPanel();
         openScreen.add(panel);
         panel.setBackground(Color.black);
         panel.setLayout(null);
 
-
-
         //Title
         JLabel title = new JLabel("Popkorn Tid");
         title.setForeground(Color.gray);
-        title.setBounds(630, 250, 300, 50);
         title.setFont(title.getFont().deriveFont(0,40));
         panel.add(title);
 
         //Picture
         ImageIcon imageIcon = new ImageIcon(getClass().getResource("/Popcorn_Time_logo.png"));
         JLabel label = new JLabel();
-        label.setBounds(630, 300, 256, 256);
         label.setIcon(imageIcon);
         panel.add(label);
 
@@ -182,13 +202,6 @@ List<Medier> seriesList;
         t.start();
 
     }
-
-
-
-
-
-
-
 }
 
 
