7f64030b8bc69af9e1aaba3cac4978ebcf6b08e8
mariusstokkebro <marius007@hotmail.dk>
2022-12-12 14:46:18 +0100

    UI START

--- Source/src/Serie.java
+++ Source/src/Serie.java
@@ -1,11 +1,8 @@
 package src;
 
 public class Serie  extends Medier{
-    private String name;
-    private int year;
-    private String[] genre;
-    private double rating;
-    private String episode;
+   private String episode;
+
 
     public Serie(String name, String year, String genre, double rating, String episode) {
         super(name,year,genre,rating);
